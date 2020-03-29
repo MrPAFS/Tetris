46ed93d007dd4923c8cf8f19b4f021173e0ba5cf
Pedro Antonio <45101558+MrPAFS@users.noreply.github.com>
2020-03-29 12:50:15 -0300

    Correção de um pequeno erro
    Não muda muita coisa, apenas excluiu a linhas que continha "pass"

--- EnvironmentExceptions.py
+++ EnvironmentExceptions.py
@@ -4,7 +4,6 @@
     É chamada toda vez que tenta-se executar uma ação que não esta no escopo de ações possíveis de um determinado ambiente
 """
 class InvalidAction(Exception):
-    pass
     def __init__(self, invalid_action, valid_actions):
         self.message = "{} is invalid, instead of this use only one of these actions: {}".format(invalid_action, valid_actions)
 
